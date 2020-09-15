34d424724575e18f9103014dd55ad5e1ccc7268c
jwoonge <511sjw@naver.com>
2020-09-15 18:55:28 +0900

    02 plot the loss curve

--- 02/02.py
+++ 02/02.py
@@ -7,35 +7,55 @@ y_train = data[:,1].reshape([data.shape[0],1])
 global n
 n = data.shape[0]
 
-w = np.zeros(np.shape(data)[1])
+#w = np.zeros(np.shape(data)[1])
 def f_pred(X, w):
     return np.sum(X * w)
 
-y_pred = f_pred(x_train, w)
+#y_pred = f_pred(x_train, w)
 
 def loss_(y_pred, y):
     return np.average(np.square(y_pred-y))
-def loss_mse(y_pred, y):
-    return (np.matmul((y_pred-y).T, (y_pred-y))/n)[0][0]
+def loss(y_pred, y):
+    return ((np.matmul((y_pred-y).T, (y_pred-y)) )/n)[0][0]
 
-loss = loss_mse(y_pred, y_train)
-print(loss)
-print(loss_(y_pred, y_train))
-
-def grad_loss(y_pred, y, X):
-
-    return n
-
-def grad_desc(X, y, w):
+def gradient(y_pred, y, X):
+    return ((2*np.matmul(np.ones((1,n)), (y_pred-y)*X))/n)[0]
+    #return 2*np.average((y_pred-y)*X, axis=0)
+'''
+def gradient(X, y, w):
     return 2*np.average((f_pred(X,w)-y)*X, axis=0)
+'''
+def grad_desc(X, y, w_init, learning_rate, max_iter):
+    weights = [w_init]
+    w = w_init
+    y_pred = f_pred(X, w)
+    loss_train = [loss(y_pred, y)]
 
-def grad_desc2(X, y, w):
-    return 2*np.matmul(np.ones((1,n)) , ((f_pred(X,w)-y)* X))/n
+    for i in range(max_iter):
+        grad_f = gradient(y_pred, y, X)
 
+        w = w - learning_rate*grad_f
+        y_pred = f_pred(X, w)
+        loss_train.append(loss(y_pred, y))
 
-print(grad_desc(x_train, y_train, w))
-print(grad_desc2(x_train, y_train, w))
+        weights.append(w)
+    return w, loss_train, weights
+
+import time
+start = time.time()
+w_init = np.ones(np.shape(data)[1])
+learning_rate = 0.000001
+max_iter = 100
+
+w, loss_train, weights = grad_desc(x_train, y_train, w_init, learning_rate, max_iter)
+print('Time=',time.time()-start)
+print(loss_train[-1])
+print(weights[-1])
 
 # Plot the training data points
 plt.scatter(x_train[:,1], y_train)
+plt.show()
+
+# Plot the loss curve in the course of gradient descent
+plt.plot(loss_train)
 plt.show()
