c56e9298bf25dd91d8c15fcd40d8829d08013fbd
jwoonge <511sjw@naver.com>
2020-09-21 14:37:45 +0900

    03 compute the gradient

--- 03/03.py
+++ 03/03.py
@@ -28,18 +28,36 @@ def f(xs, degree):
 def f_pred(X, w):
     return np.dot(X, w)
 
-w = np.zeros((10,1))
-degrees = np.zeros((10,2))
-
-X = calX(data_noisy, degrees)
-z_pred = f_pred(X, w)
-
 def loss(v_pred, v_origin):
     return np.average(np.square(v_pred-v_origin))
 
+def gradient(v_pred, v_origin, X):
+    return 2/n * np.dot(X.T, (v_pred-v_origin))
+
+def grad_desc(X, label, w_init, tau, max_iter):
+    weights = [w_init]
+    w = w_init
+    v_pred = f_pred(X, w)
+    loss_train = [loss(v_pred, label)]
+    for i in range(max_iter):
+        grad = gradient(v_pred, label, X)
+        w = w - tau*grad
+        v_pred = f_pred(X, w)
+        loss_train.append(loss(v_pred, label))
+        weights.append(w)
+    return w, loss_train, np.array(weights)
+
 label = data_noisy[:,-1].reshape((n,1))
-print(loss(z_pred, label))
-'''
+degrees = np.array([[0,0], [1,0], [0,1], [1,1], [2,0], [0,2], [2,1], [1,2], [2,2], [4,4]])
+w_init = np.ones((10,1))
+X = calX(data_noisy, degrees)
+tau = 0.1
+max_iter = 1000
+
+w, loss_train, weights = grad_desc(X, label, w_init, tau, max_iter)
+
+
+
 ### Plot the clean data in 3D cartesian coordinate system ###
 fig = plt.figure()
 ax = fig.gca(projection='3d')
@@ -53,6 +71,8 @@ ax.scatter(data_noisy[:,0], data_noisy[:,1], data_noisy[:,2], color='r')
 plt.show()
 
 ### Plot the loss curve in the course of gradient descent ###
+plt.plot(loss_train)
+plt.show()
 
 ### Print out the final loss value at convergence of the gradient descent ###
 
@@ -62,4 +82,3 @@ plt.show()
 
 ### Plot the prediction functions superimposed on the training data ###
 
-'''
