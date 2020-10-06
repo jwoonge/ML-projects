8dcf482fd1e463a807c9ccb84b17d8c0f168e595
jwoonge <511sjw@naver.com>
2020-10-06 18:14:05 +0900

    05-a Vectorize input data

--- 05/05-a.py
+++ 05/05-a.py
@@ -4,16 +4,26 @@ from mpl_toolkits.mplot3d import axes3d
 
 # 1. Load dataset
 data = np.loadtxt('dataset-a.txt', delimiter=',')
-n = data.shape[0]
+global n; n = data.shape[0]
+global degrees; degrees = np.array([[0,0],[1,0],[0,1],[2,0],[0,2]])
+global nd; nd = degrees.shape[0]
+global nx; nx = data.shape[1]-1
 
 # 2. Define a logistic regression loss function and its gradient
-def segmoid(z):
+def sigmoid(z):
     return 1/(1+np.exp(-z))
 
+def vectorize(xs):
+    X = np.ones([xs.shape[0], nd])
+    for i in range(nd):
+        X[:,i] = f(xs, degrees[i])
+    return X
 
-
-
-
+def f(xs, degree):
+    ret = np.ones((xs.shape[0]))
+    for i in range(nx):
+        ret *= (xs[:,i]**degree[i])
+    return ret
 
 ###### RESULTS ######
 # 01 Visualize the data
