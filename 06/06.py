d240960c21c657fcd526022ba03840b0f760a3bb
jwoonge <511sjw@naver.com>
2020-10-13 16:58:02 +0900

    06 Read and Plot the data

---
+++ 06/06.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+
+data_train = np.loadtxt('training.txt', delimiter=',')
+data_test = np.loadtxt('testing.txt', delimiter=',')
+
+
+
+
+def plot_data(data, title, xmin=-2,xmax=3,ymin=-1,ymax=1.2):
+    plt.scatter(data[data[:,2]==0,0],data[data[:,2]==0,1], c='r', s=5)
+    plt.scatter(data[data[:,2]==1,0],data[data[:,2]==1,1], c='b', s=5)
+    plt.xlim(xmin, xmax)
+    plt.ylim(ymin, ymax)
+    plt.legend(['label=0','label=1'])
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title(title)
+###### OUTPUT ######
+# 1. Plot the training data
+plot_data(data_train, 'training data')
+plt.show()
+# 2. Plot the testing data
+plot_data(data_test, 'testing data')
+plt.show()
