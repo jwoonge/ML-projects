cbe1587d241006518b89759c071de7ceab5fbb4c
jwoonge <511sjw@naver.com>
2020-11-03 15:53:33 +0900

    07 compute label

--- 07/k-means clustering.py
+++ 07/k-means clustering.py
@@ -6,20 +6,26 @@ dataset = pd.read_csv('data-kmeans.csv')
 data = dataset.values
 
 def compute_distance(a, b):
-    return np.sqrt(np.sum(np.square(a-b)))
+    return np.sqrt(np.sum(np.square(a-b), axis=-1))
 
 def compute_centroid(Z):
     return np.average(Z, axis=0)
 
-a = np.array([[0,1],[1,1],[0,0]])
-print(compute_centroid(a))
+def compute_label(z, M):
+    return np.argmin(compute_distance(z, M))
+
+z = [2,2]
+M = [[1,1],[2,2],[3,3]]
+z = np.array(z); M = np.array(M)
+print(compute_distance(z, M))
+print(compute_label(z, M))
+print(compute_distance(z, np.array([3,3])))
 
 
 
 
 
-
-
+'''
 ### RESULTS ###
 # 01 Plot the data points
 plt.scatter(data[:,0], data[:,1], label='data')
@@ -34,3 +40,4 @@ plt.show()
 # 04 Plot the centroid of each cluster
 
 # 05 Plot the final clustering result
+'''
