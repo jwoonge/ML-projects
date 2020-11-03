13be8eb4dae7a1e2955b0f82c529cbfc9f6259ed
jwoonge <511sjw@naver.com>
2020-11-03 17:56:58 +0900

    07 debug warnings

--- 07/k-means clustering.py
+++ 07/k-means clustering.py
@@ -33,13 +33,23 @@ def compute_loss(C, M):
         cluster = data[C==i]
         dists = compute_distance(cluster, M[i])
         loss += np.sum(dists)
-    return loss/k
+    return loss/len(data)
 
 def random_initialize():
     labels = np.zeros(len(data))
     for i in range(len(data)):
         labels[i] = np.random.randint(k)
     return labels
+
+def check_valid_initial(label):
+    cnt = [0 for x in range(k)]
+    for l in label:
+        cnt[int(l)] += 1
+    print(cnt)
+    for i in range(k):
+        if cnt[i]==0:
+            return False
+    return True
 
 import copy
 def k_means_clustering():
@@ -49,37 +59,38 @@ def k_means_clustering():
     
     for t in range(100):
         labels = random_initialize()
-        labels_it = [copy.deepcopy(labels)]
-        loss_it = []
-        centroids_it = []
-        break_cnt = 0
-        while True:
-            # calculate centroids
-            centroids = []
-            for i in range(k):
-                centroids.append(compute_centroid(data[labels==i]))
-            centroids = np.array(centroids)
-            # update labels
-            labels = np.zeros(len(data))
-            for i in range(len(data)):
-                labels[i] = compute_label(data[i,:], centroids)
-            
-            loss_it.append(compute_loss(labels, centroids))
-            centroids_it.append(centroids)
-            labels_it.append(labels)
-            if len(loss_it) > 1 and loss_it[-1]==loss_it[-2]:
-                break_cnt += 1
-            if break_cnt >10:
-                break
-        if t==0:
-            bst_loss = copy.deepcopy(loss_it)
-            bst_centroids = copy.deepcopy(centroids_it)
-            bst_labels = copy.deepcopy(labels_it)
-        else:
-            if bst_loss[-1] > loss_it[-1]:
+        if check_valid_initial(labels):
+            labels_it = [copy.deepcopy(labels)]
+            loss_it = []
+            centroids_it = []
+            break_cnt = 0
+            while True:
+                # calculate centroids
+                centroids = []
+                for i in range(k):
+                    centroids.append(compute_centroid(data[labels==i]))
+                centroids = np.array(centroids)
+                # update labels
+                labels = np.zeros(len(data))
+                for i in range(len(data)):
+                    labels[i] = compute_label(data[i,:], centroids)
+                
+                loss_it.append(compute_loss(labels, centroids))
+                centroids_it.append(centroids)
+                labels_it.append(labels)
+                if len(loss_it) > 1 and loss_it[-1]==loss_it[-2]:
+                    break_cnt += 1
+                if break_cnt >10:
+                    break
+            if t==0:
                 bst_loss = copy.deepcopy(loss_it)
                 bst_centroids = copy.deepcopy(centroids_it)
                 bst_labels = copy.deepcopy(labels_it)
+            else:
+                if bst_loss[-1] > loss_it[-1]:
+                    bst_loss = copy.deepcopy(loss_it)
+                    bst_centroids = copy.deepcopy(centroids_it)
+                    bst_labels = copy.deepcopy(labels_it)
     bst_centroids = np.array(bst_centroids)
     bst_centroids_distance = compute_distance([0,0], bst_centroids)
     return bst_labels, bst_loss, bst_centroids, bst_centroids_distance
@@ -91,7 +102,7 @@ def plot_clusters(labels, centroid):
     color_it = iter(cm.rainbow(np.linspace(0,1,k)))
     for i in range(k):
         cluster = data[labels==i]
-        color = next(color_it)
+        color = next(color_it).reshape(1,-1)
         plt.scatter(cluster[:,0], cluster[:,1], label='Cluster '+str(i), c=color)
     plt.scatter(centroid[:,0], centroid[:,1], marker='X', label='Centroids')
     plt.legend()
