68cc2db2b83fe4f1eb7d37a79d59dfdc01cab534
jwoonge <511sjw@naver.com>
2020-11-03 17:32:46 +0900

    plot the loss

--- 07/k-means clustering.py
+++ 07/k-means clustering.py
@@ -43,13 +43,13 @@ def random_initialize():
 
 import copy
 def k_means_clustering():
-    bst_initial = []
     bst_loss = []
     bst_centroids = []
     bst_labels = []
+    
     for t in range(100):
         labels = random_initialize()
-        initial = copy.deepcopy(labels)
+        labels_it = [copy.deepcopy(labels)]
         loss_it = []
         centroids_it = []
         break_cnt = 0
@@ -65,34 +65,37 @@ def k_means_clustering():
                 labels[i] = compute_label(data[i,:], centroids)
             
             loss_it.append(compute_loss(labels, centroids))
-            centroids_it.append
+            centroids_it.append(centroids)
+            labels_it.append(labels)
             if len(loss_it) > 1 and loss_it[-1]==loss_it[-2]:
                 break_cnt += 1
             if break_cnt >10:
                 break
-        if t==0:    
-            bst_initial = copy.deepcopy(initial)
+        if t==0:
             bst_loss = copy.deepcopy(loss_it)
             bst_centroids = copy.deepcopy(centroids_it)
-            bst_labels = copy.deepcopy(labels)
+            bst_labels = copy.deepcopy(labels_it)
         else:
             if bst_loss[-1] > loss_it[-1]:
-                bst_initial = copy.deepcopy(initial)
                 bst_loss = copy.deepcopy(loss_it)
                 bst_centroids = copy.deepcopy(centroids_it)
-                bst_labels = copy.deepcopy(labels)
+                bst_labels = copy.deepcopy(labels_it)
+    bst_centroids = np.array(bst_centroids)
+    bst_centroids_distance = compute_distance([0,0], bst_centroids)
+    return bst_labels, bst_loss, bst_centroids, bst_centroids_distance
 
-    return bst_initial, bst_loss, bst_centroids, bst_labels
 
+labels, loss, centroids, centroid_distances = k_means_clustering()
 
-initial, loss, centroids, labels = k_means_clustering()
-
-def plot_clusters(labels):
+def plot_clusters(labels, centroid):
     color_it = iter(cm.rainbow(np.linspace(0,1,k)))
     for i in range(k):
         cluster = data[labels==i]
         color = next(color_it)
-        plt.scatter(cluster[:,0], cluster[:,1], label='cluster :'+str(i), c=color)
+        plt.scatter(cluster[:,0], cluster[:,1], label='Cluster '+str(i), c=color)
+    plt.scatter(centroid[:,0], centroid[:,1], marker='X', label='Centroids')
+    plt.legend()
+    plt.title('cluster')
     plt.show()
 
 ### RESULTS ###
@@ -103,13 +106,21 @@ plt.legend()
 plt.show()
 
 # 02 Visualize the initial condition of the point labels
-plot_clusters(initial)
+plot_clusters(labels[0], centroids[0])
 
 # 03 Plot the loss curve
 plt.plot(loss)
+plt.title('loss')
 plt.show()
 
 # 04 Plot the centroid of each cluster
+color_it = iter(cm.rainbow(np.linspace(0,1,k)))
+for i in range(k):
+    color = next(color_it)
+    plt.plot(centroid_distances[:,i], label='Cluster '+str(i), c=color)
+plt.legend()
+plt.title('centroid of cluster')
+plt.show()
 
 # 05 Plot the final clustering result
-plot_clusters(labels)
+plot_clusters(labels[-1], centroids[-1])
