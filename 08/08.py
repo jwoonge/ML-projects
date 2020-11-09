0320529ba10d6a23c749f5fa6feb6780505ac798
jwoonge <511sjw@naver.com>
2020-11-10 02:36:02 +0900

    08 compute principal direction

--- 08/08.py
+++ 08/08.py
@@ -19,7 +19,28 @@ def compute_covariance(data):
     return np.matmul(data.T, data)/data.shape[0]
 
 covar = compute_covariance(data)
-print(np.shape(covar))
+
+def compute_principal_direction(covariance):
+    eig_value, eig_vector = np.linalg.eig(covariance)
+    print(eig_value, eig_vector)
+    tmp = []
+    dim = len(eig_value)
+    for i in range(dim):
+        tmp.append([eig_value[i], eig_vector[i]])
+    tmp.sort(key=lambda x:x[0], reverse=True)
+    eig_value = []; eig_vector = []
+    for i in range(dim):
+        eig_value.append(tmp[i][0])
+        eig_vector.append(tmp[i][1])
+    return np.array(eig_value), np.array(eig_vector)
+
+a, b = compute_principal_direction(covar)
+
+
+def compute_projection(point, axis):
+    return None
+def compute_distance(p1, p2):
+    return np.sqrt(np.sum(np.square(p1-p2)))
 
 
 
