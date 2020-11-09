b22e104056064520a792f9379d05947d499c6d40
jwoonge <511sjw@naver.com>
2020-11-10 02:01:47 +0900

    08 read data and plot the original data

---
+++ 08/08.py
@@ -0,0 +1,33 @@
+## Principal Component Analysis (PCA) ##
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt('data-pca.txt', delimiter=',')
+x = data[:,0]
+y = data[:,1]
+
+
+
+
+
+
+## Results ##
+# 1. Plot the origintal data points
+plt.scatter(x, y, c='r', s=3)
+plt.show()
+
+# 2. Plot the normalized data points
+
+# 3. Plot the principal axis
+
+# 4. Plot the first principal axis
+
+# 5. Plot the project of the normalized data points onto the fist principal axis
+
+# 6. Plot the lines between the normalized data points and their projection points on the fist principal axis
+
+# 7. Plot the second principal axis
+
+# 8. Plot the project of the normalized data points onto the second principal axis
+
+# 9. Plot the lines between the normalized data points and their projection points on the second principal axis
