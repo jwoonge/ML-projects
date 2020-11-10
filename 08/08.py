e14dd012a3d38b70fc54b47de042efffe8fe733b
jwoonge <511sjw@naver.com>
2020-11-10 19:36:26 +0900

    08 plot the distance

--- 08/08.py
+++ 08/08.py
@@ -4,8 +4,6 @@ import matplotlib.pyplot as plt
 
 data = np.loadtxt('data-pca.txt', delimiter=',')
 #data = np.matmul(data, np.array([[0.85,-0.5],[0.5,0.85]]))
-x = data[:,0]
-y = data[:,1]
 
 def normalize_data(data):
     mean = np.average(data, axis=0)
@@ -45,8 +43,10 @@ def projection(point, axis):
     ks = ks.reshape((len(ks),1))
     return np.matmul(ks, axis.reshape(1,len(axis)))
 
-projected = projection(data_norm, axis[0,:])
-print(projected.shape)
+projected1 = projection(data_norm, axis[0,:])
+projected2 = projection(data_norm, axis[1,:])
+slope1 = axis[0][1]/axis[0][0]
+slope2 = axis[1][1]/axis[1][0]
 
 def distance(p1, p2):
     return np.sqrt(np.sum(np.square(p1-p2)))
@@ -56,7 +56,8 @@ def distance(p1, p2):
 ## Results ##
 # 1. Plot the origintal data points
 plt.figure(figsize=(6,6))
-plt.scatter(x, y, c='r', s=3)
+plt.scatter(data[:,0], data[:,1], c='r', s=3)
+plt.title('original data points')
 plt.show()
 
 # 2. Plot the normalized data points
@@ -65,6 +66,7 @@ plt.figure(figsize=(6,6))
 plt.scatter(x_norm, y_norm, c='r', s=3) 
 plt.xlim(arange)
 plt.ylim(arange)
+plt.title('data normalized by z-scoring')
 plt.show()
 
 # 3. Plot the principal axis
@@ -74,16 +76,17 @@ plt.xlim(arange)
 plt.ylim(arange)
 plt.plot([0, axis[0][0]], [0, axis[0][1]], c='b')
 plt.plot([0, axis[1][0]], [0, axis[1][1]], c='g')
-#origin = np.zeros((2,2))
-#plt.quiver(*origin, axis[:,0], axis[:,1], color=['b','g'])
+plt.title('principal diretions')
 plt.show()
+
 # 4. Plot the first principal axis
-slope = axis[0][1]/axis[0][0]
+
+
 plt.figure(figsize=(6,6))
 plt.scatter(x_norm, y_norm, c='r', s=3)
 plt.xlim(arange)
 plt.ylim(arange)
-plt.plot(arange,arange*slope, c='b')
+plt.plot(arange,arange*slope1, c='b')
 plt.title('first principle axis')
 plt.show()
 
@@ -92,15 +95,53 @@ plt.figure(figsize=(6,6))
 plt.scatter(x_norm, y_norm, c='r', s=3)
 plt.xlim(arange)
 plt.ylim(arange)
-plt.plot(arange, arange*slope, c='b')
-plt.scatter(projected[:,0], projected[:,1], c='g')
+plt.plot(arange, arange*slope1, c='b')
+plt.scatter(projected1[:,0], projected1[:,1], c='g')
+plt.title('projection to the first principle axis')
+plt.show()
+
+# 6. Plot the lines between the normalized data points and their projection points on the fist principal axis
+plt.figure(figsize=(6,6))
+plt.scatter(x_norm, y_norm, c='r', s=3)
+plt.xlim(arange)
+plt.ylim(arange)
+plt.plot(arange, arange*slope1, c='b')
+plt.scatter(projected1[:,0], projected1[:,1], c='g')
+for i in range(len(data_norm)):
+    plt.plot([projected1[i,0], data_norm[i,0]], [projected1[i,1], data_norm[i,1]])
+
+plt.title('distance to the first principal axis')
+plt.show()
+
+# 7. Plot the second principal axis
+plt.figure(figsize=(6,6))
+plt.scatter(x_norm, y_norm, c='r', s=3)
+plt.xlim(arange)
+plt.ylim(arange)
+plt.plot(arange,arange*slope2, c='b')
+plt.title('second principle axis')
+plt.show()
+
+# 8. Plot the project of the normalized data points onto the second principal axis
+plt.figure(figsize=(6,6))
+plt.scatter(x_norm, y_norm, c='r', s=3)
+plt.xlim(arange)
+plt.ylim(arange)
+plt.plot(arange, arange*slope2, c='b')
+plt.scatter(projected2[:,0], projected2[:,1], c='g')
+plt.title('projection to the second principle axis')
 plt.show()
 
 
-# 6. Plot the lines between the normalized data points and their projection points on the fist principal axis
-
-# 7. Plot the second principal axis
-
-# 8. Plot the project of the normalized data points onto the second principal axis
-
 # 9. Plot the lines between the normalized data points and their projection points on the second principal axis
+plt.figure(figsize=(6,6))
+plt.scatter(x_norm, y_norm, c='r', s=3)
+plt.xlim(arange)
+plt.ylim(arange)
+plt.plot(arange, arange*slope1, c='b')
+plt.scatter(projected2[:,0], projected2[:,1], c='g')
+for i in range(len(data_norm)):
+    plt.plot([projected2[i,0], data_norm[i,0]], [projected2[i,1], data_norm[i,1]])
+
+plt.title('distance to the second principal axis')
+plt.show()
